16894a3c95e0949039fff176712d5eaa4c7c69a1
wang xiang <sdwxiang@hotmail.com>
2018-03-10 09:15:41 +0800

    use singlefragment base class

--- app/src/main/java/work/wangxiang/localvideo/view/VideoListActivity.java
+++ app/src/main/java/work/wangxiang/localvideo/view/VideoListActivity.java
@@ -5,23 +5,12 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import work.wangxiang.android.view.SingleFragmentActivity;
 import work.wangxiang.androiddemo.R;
 
-public class VideoListActivity extends AppCompatActivity {
-
+public class VideoListActivity extends SingleFragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video_list);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment videoListFrag = fragmentManager.findFragmentById(R.id.fragment_container);
-
-        if (videoListFrag == null) {
-            videoListFrag = new VideoListFragment();
-            fragmentManager.beginTransaction()
-                    .add(R.id.fragment_container, videoListFrag)
-                    .commit();
-        }
+    protected Fragment getFragment() {
+        return new VideoListFragment();
     }
 }
