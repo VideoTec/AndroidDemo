f0f7346782d22746688de23c3dd20195958914a9
wang xiang <sdwxiang@hotmail.com>
2018-04-06 19:25:30 +0800

    test keyboard show and hide

--- app/src/main/java/work/wangxiang/androiddemo/MainActivity.java
+++ app/src/main/java/work/wangxiang/androiddemo/MainActivity.java
@@ -3,20 +3,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 
+import com.alimuzaffar.lib.pin.PinEntryEditText;
+
+import work.wangxiang.android.common.App;
+import work.wangxiang.android.common.Utils;
 import work.wangxiang.livetv.view.LiveTvActivity;
 import work.wangxiang.localvideo.view.VideoListActivity;
 import work.wangxiang.test.recyclerview.ActivityFragmentTest;
 
 public class MainActivity extends AppCompatActivity {
+    PinEntryEditText pinEntryEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pinEntryEditText = findViewById(R.id.pinEntryEditText);
+        //pinEntryEditText.setFocusable(true);
+        //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
 
     public void onLoadLocalVideo(View view) {
-        VideoListActivity.openVideoList(this);
+        //VideoListActivity.openVideoList(this);
+        pinEntryEditText.requestFocus();
+        App.getCtx().showSoftKeyboard(pinEntryEditText);
     }
 
     public void onRecyclerViewTest(View view) {
@@ -24,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onLoadLiveTv(View view) {
-        startActivity(new Intent(this, LiveTvActivity.class));
+        //startActivity(new Intent(this, LiveTvActivity.class));
+        App.getCtx().hideSoftKeyboard(pinEntryEditText);
     }
 }
